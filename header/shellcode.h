dbd49103d8ad3f7387fce854be0e851c4046f99a
PRM6 <PRM6dev@protonmail.com>
2024-11-11 05:12:33 +0100

    Removed unused import
    Remove <iostream>

--- header/shellcode.h
+++ header/shellcode.h
@@ -1,16 +1,15 @@
 #include <windows.h>
 #include <winternl.h>
-#include <iostream>
 
 typedef NTSTATUS(WINAPI* NTQUERYINFOPROC64)(
-	HANDLE           ProcessHandle,
-	PROCESSINFOCLASS ProcessInformationClass,
-	PVOID            ProcessInformation,
-	ULONG            ProcessInformationLength,
-	PULONG_PTR       ReturnLength
-	);
+    HANDLE           ProcessHandle,
+    PROCESSINFOCLASS ProcessInformationClass,
+    PVOID            ProcessInformation,
+    ULONG            ProcessInformationLength,
+    PULONG_PTR       ReturnLength
+);
 
-//Paste your shellcode in here
+// Paste your actual shellcode here
 unsigned char shellcode[1] = {
-	0x01
+    0x01
 };
